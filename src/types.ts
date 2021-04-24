4f47206a2de21b48200f908588ebbbb0ef32c04e
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-04-24 11:01:24 -0400

    Change ghost and evidence order to match in-game

--- src/types.ts
+++ src/types.ts
@@ -1,20 +1,51 @@
 export enum Evidence {
-  Emf5 = "EMF 5",
-  Fingerprints = "Fingerprints",
-  Freezing = "Freezing Temperatures",
-  GhostOrbs = "Ghost Orbs",
-  GhostWriting = "Ghost Writing",
-  SpiritBox = "Spirit Box",
+  Emf5 = 'EMF 5',
+  SpiritBox = 'Spirit Box',
+  Fingerprints = 'Fingerprints',
+  GhostOrbs = 'Ghost Orbs',
+  GhostWriting = 'Ghost Writing',
+  Freezing = 'Freezing Temperatures',
 }
 
 export interface Ghost {
-  type: string,
-  evidence: Array<Evidence>,
-  strength: string,
-  weakness: string,
+  type: string
+  evidence: Array<Evidence>
+  strength: string
+  weakness: string
 }
 
 export const AllGhosts: Array<Ghost> = [
+  {
+    type: 'Spirit',
+    evidence: [
+      Evidence.SpiritBox,
+      Evidence.Fingerprints,
+      Evidence.GhostWriting,
+    ],
+    strength: 'Nothing.',
+    weakness:
+      'Using Smudge Sticks on a spirit will stop it attacking for a long period of time.',
+  },
+  {
+    type: 'Wraith',
+    evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.Freezing],
+    strength:
+      "Wraiths almost never touch the ground meaning it can't be tracked by footsteps.",
+    weakness: 'Wraiths have a toxic reaction to Salt.',
+  },
+  {
+    type: 'Phantom',
+    evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.Freezing],
+    strength: 'Looking at a Phantom will considerably drop your sanity.',
+    weakness:
+      'Taking a photo of the Phantom will make it temporarily disappear.',
+  },
+  {
+    type: 'Poltergeist',
+    evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.GhostOrbs],
+    strength: 'A Poltergeist can throw huge amounts of objects at once.',
+    weakness: 'A Poltergeist is almost ineffective in an empty room.',
+  },
   {
     type: 'Banshee',
     evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.Freezing],
@@ -23,45 +54,18 @@ export const AllGhosts: Array<Ghost> = [
       'Banshees fear the Crucifix and will be less aggressive when near one.',
   },
   {
-    type: 'Demon',
-    evidence: [Evidence.Freezing, Evidence.GhostWriting, Evidence.SpiritBox],
-    strength: 'Demons will attack more often than any other Ghost.',
-    weakness:
-      "Asking a Demon successful questions on the Ouija Board won't lower the users sanity.",
-  },
-  {
     type: 'Jinn',
-    evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.SpiritBox],
+    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostOrbs],
     strength: 'A Jinn will travel at a faster speed if its victim is far away.',
     weakness:
       'Turning off the locations power source will prevent the Jinn from using its ability.',
   },
   {
     type: 'Mare',
-    evidence: [Evidence.Freezing, Evidence.GhostOrbs, Evidence.SpiritBox],
+    evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.Freezing],
     strength: 'A Mare will have an increased chance to attack in the dark.',
     weakness:
       'Turning the lights on around the Mare will lower its chance to attack.',
-  },
-  {
-    type: 'Oni',
-    evidence: [Evidence.Emf5, Evidence.GhostWriting, Evidence.SpiritBox],
-    strength:
-      "Oni's are more active when people are nearby and have been seen moving objects at great speed.",
-    weakness: 'Being more active make the Oni easier to find and identify.',
-  },
-  {
-    type: 'Phantom',
-    evidence: [Evidence.Emf5, Evidence.Freezing, Evidence.GhostOrbs],
-    strength: 'Looking at a Phantom will considerably drop your sanity.',
-    weakness:
-      'Taking a photo of the Phantom will make it temporarily disappear.',
-  },
-  {
-    type: 'Poltergeist',
-    evidence: [Evidence.GhostOrbs, Evidence.Fingerprints, Evidence.SpiritBox],
-    strength: 'A Poltergeist can throw huge amounts of objects at once.',
-    weakness: 'A Poltergeist is almost ineffective in an empty room.',
   },
   {
     type: 'Revenant',
@@ -78,29 +82,25 @@ export const AllGhosts: Array<Ghost> = [
       'The Ghost will not enter hunting mode if there is multiple people nearby.',
   },
   {
-    type: 'Spirit',
-    evidence: [
-      Evidence.GhostWriting,
-      Evidence.Fingerprints,
-      Evidence.SpiritBox,
-    ],
-    strength: 'Nothing.',
+    type: 'Demon',
+    evidence: [Evidence.SpiritBox, Evidence.GhostWriting, Evidence.Freezing],
+    strength: 'Demons will attack more often than any other Ghost.',
     weakness:
-      'Using Smudge Sticks on a spirit will stop it attacking for a long period of time.',
-  },
-  {
-    type: 'Wraith',
-    evidence: [Evidence.Fingerprints, Evidence.Freezing, Evidence.SpiritBox],
-    strength:
-      "Wraiths almost never touch the ground meaning it can't be tracked by footsteps.",
-    weakness: 'Wraiths have a toxic reaction to Salt.',
+      "Asking a Demon successful questions on the Ouija Board won't lower the users sanity.",
   },
   {
     type: 'Yurei',
-    evidence: [Evidence.Freezing, Evidence.GhostOrbs, Evidence.GhostWriting],
+    evidence: [Evidence.GhostOrbs, Evidence.GhostWriting, Evidence.Freezing],
     strength:
       "Yurei's have been known to have a stronger effect on people sanity.",
     weakness:
       "Smudging the Yurei's room will cause it to not wander around the location for a long time.",
+  },
+  {
+    type: 'Oni',
+    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostWriting],
+    strength:
+      "Oni's are more active when people are nearby and have been seen moving objects at great speed.",
+    weakness: 'Being more active make the Oni easier to find and identify.',
   },
 ]
