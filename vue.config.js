126448aec93bdd9e99d3fa13dcf224e6da5d1570
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-04-23 20:05:10 -0400

    Fix configs

--- vue.config.js
+++ vue.config.js
@@ -1,2 +1,3 @@
-export const publicPath =
-  process.env.NODE_ENV === 'production' ? '/phasjournal/' : '/'
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/phasjournal/' : '/',
+}
