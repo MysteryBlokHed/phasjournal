79af24aded29b540147aa064d2774e0ac1e60e7a
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-04-28 16:04:40 -0400

    Shorten ghost strengths/weaknesses

--- src/types.ts
+++ src/types.ts
@@ -22,85 +22,74 @@ export const AllGhosts: Array<Ghost> = [
       Evidence.Fingerprints,
       Evidence.GhostWriting,
     ],
-    strength: 'Nothing.',
-    weakness:
-      'Using Smudge Sticks on a spirit will stop it attacking for a long period of time.',
+    strength: 'None',
+    weakness: 'Smudge sticks will stop attacks for a long period of time',
   },
   {
     type: 'Wraith',
     evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.Freezing],
-    strength:
-      "Wraiths almost never touch the ground meaning it can't be tracked by footsteps.",
-    weakness: 'Wraiths have a toxic reaction to Salt.',
+    strength: `Can't be tracked by footsteps`,
+    weakness: 'Toxic reaction to salt',
   },
   {
     type: 'Phantom',
     evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.Freezing],
-    strength: 'Looking at a Phantom will considerably drop your sanity.',
-    weakness:
-      'Taking a photo of the Phantom will make it temporarily disappear.',
+    strength: 'Looking at a it significantly lowers your sanity',
+    weakness: 'Taking a photo will make it go away for some time',
   },
   {
     type: 'Poltergeist',
     evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.GhostOrbs],
-    strength: 'A Poltergeist can throw huge amounts of objects at once.',
-    weakness: 'A Poltergeist is almost ineffective in an empty room.',
+    strength: 'Can throw many objects at once',
+    weakness: 'Almost ineffective in an empty room',
   },
   {
     type: 'Banshee',
     evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.Freezing],
-    strength: 'A Banshee will only target one person at a time.',
-    weakness:
-      'Banshees fear the Crucifix and will be less aggressive when near one.',
+    strength: 'Only targets one person at a time',
+    weakness: 'Less active near a Crucifix',
   },
   {
     type: 'Jinn',
     evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostOrbs],
-    strength: 'A Jinn will travel at a faster speed if its victim is far away.',
-    weakness:
-      'Turning off the locations power source will prevent the Jinn from using its ability.',
+    strength: 'Travels faster when its victim is farther away',
+    weakness: 'Disabling the breaker disables its ability',
   },
   {
     type: 'Mare',
     evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.Freezing],
-    strength: 'A Mare will have an increased chance to attack in the dark.',
-    weakness:
-      'Turning the lights on around the Mare will lower its chance to attack.',
+    strength: 'Increased chance of attacking in the dark',
+    weakness: 'Turning lights on will make it less likely to attack',
   },
   {
     type: 'Revenant',
     evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.GhostWriting],
-    strength:
-      'A Revenant will travel at a significantly faster speed when hunting a victim.',
-    weakness: 'Hiding from the Revenant will cause it to move very slowly.',
+    strength: 'Travels faster while hunting',
+    weakness: 'Hiding will make it move much slower',
   },
   {
     type: 'Shade',
     evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.GhostWriting],
-    strength: 'Being shy means the Ghost will be harder to find.',
-    weakness:
-      'The Ghost will not enter hunting mode if there is multiple people nearby.',
+    strength: 'Being shy makes the ghost harder to find',
+    weakness: `Doesn't hunt with multiple people nearby`,
   },
   {
     type: 'Demon',
     evidence: [Evidence.SpiritBox, Evidence.GhostWriting, Evidence.Freezing],
-    strength: 'Demons will attack more often than any other Ghost.',
-    weakness:
-      "Asking a Demon successful questions on the Ouija Board won't lower the users sanity.",
+    strength: 'Attacks more than any other ghost',
+    weakness: 'Answered Ouija Board questions do not lower sanity',
   },
   {
     type: 'Yurei',
     evidence: [Evidence.GhostOrbs, Evidence.GhostWriting, Evidence.Freezing],
-    strength:
-      "Yurei's have been known to have a stronger effect on people sanity.",
+    strength: 'Has a strong effect on sanity',
     weakness:
-      "Smudging the Yurei's room will cause it to not wander around the location for a long time.",
+      'Using Smudge Sticks in the ghost room will stop it wandering for some time',
   },
   {
     type: 'Oni',
     evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostWriting],
-    strength:
-      "Oni's are more active when people are nearby and have been seen moving objects at great speed.",
-    weakness: 'Being more active make the Oni easier to find and identify.',
+    strength: 'More active with more people and can moves objects fast',
+    weakness: 'Being more active makes it easier to find',
   },
 ]
