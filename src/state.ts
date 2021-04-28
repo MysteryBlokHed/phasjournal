7fbce78ffc518c29cb1f5774fdd80d88295ca334
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-04-28 12:58:56 -0400

    Update state debugging options

--- src/state.ts
+++ src/state.ts
@@ -2,7 +2,7 @@ import { Ghost, AllGhosts, Evidence } from './types'
 import { reactive } from 'vue'
 
 const store = {
-  debug: false,
+  debugLevel: 0,
 
   state: reactive({
     ghosts: AllGhosts.slice(),
@@ -13,60 +13,123 @@ const store = {
   }),
 
   setGhosts(newValue: Ghost[]): void {
-    if (this.debug) console.log('setGhosts called with new value', newValue)
+    if (this.debugLevel === 1)
+      console.log('setGhosts called with new value', newValue)
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('setGhosts called with new value', newValue)
+      console.trace()
+      console.groupEnd()
+    }
     this.state.ghosts.length = 0
     this.state.ghosts.push(...newValue)
   },
 
   clearGhosts(): void {
-    if (this.debug) console.log('clearGhosts called')
+    if (this.debugLevel === 1) console.log('clearGhosts called')
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('clearGhosts called')
+      console.trace()
+      console.groupEnd()
+    }
     this.state.ghosts.length = 0
   },
 
   setEvidencePresent(newValue: Evidence[]): void {
-    if (this.debug)
+    if (this.debugLevel === 1)
       console.log('setEvidencePresent called with new value', newValue)
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed(
+        'setEvidencePresent called with new value',
+        newValue
+      )
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidencePresent.length = 0
     this.state.evidencePresent.push(...newValue)
   },
 
   clearEvidencePresent(): void {
-    if (this.debug) console.log('clearEvidencePresent called')
+    if (this.debugLevel === 1) console.log('clearEvidencePresent called')
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('clearEvidencePresent called')
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidencePresent.length = 0
   },
 
   setEvidenceNotPresent(newValue: Evidence[]): void {
-    if (this.debug)
+    if (this.debugLevel === 1)
       console.log('setEvidenceNotPresent called with new value', newValue)
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed(
+        'setEvidenceNotPresent called with new value',
+        newValue
+      )
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceNotPresent.length = 0
     this.state.evidenceNotPresent.push(...newValue)
   },
 
   clearEvidenceNotPresent(): void {
-    if (this.debug) console.log('clearEvidenceNotPresent called')
+    if (this.debugLevel === 1) console.log('clearEvidenceNotPresent called')
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('clearEvidenceNotPresent called')
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceNotPresent.length = 0
   },
 
   setEvidenceInCommon(newValue: Evidence[]): void {
-    if (this.debug)
+    if (this.debugLevel === 1)
       console.log('setEvidenceInCommon called with new value', newValue)
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed(
+        'setEvidenceInCommon called with new value',
+        newValue
+      )
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceInCommon.length = 0
     this.state.evidenceInCommon.push(...newValue)
   },
 
   clearEvidenceInCommon(): void {
-    if (this.debug) console.log('clearEvidenceInCommon called')
+    if (this.debugLevel === 1) console.log('clearEvidenceInCommon called')
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('clearEvidenceInCommon called')
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceInCommon.length = 0
   },
   setEvidenceNeeded(newValue: Evidence[]): void {
-    if (this.debug)
+    if (this.debugLevel === 1)
       console.log('setEvidenceNeeded called with new value', newValue)
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed(
+        'setEvidenceNeeded called with new value',
+        newValue
+      )
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceNeeded.length = 0
     this.state.evidenceNeeded.push(...newValue)
   },
 
   clearEvidenceNeeded(): void {
-    if (this.debug) console.log('clearEvidenceNeeded called')
+    if (this.debugLevel === 1) console.log('clearEvidenceNeeded called')
+    else if (this.debugLevel === 2) {
+      console.groupCollapsed('clearEvidenceNeeded called')
+      console.trace()
+      console.groupEnd()
+    }
     this.state.evidenceNeeded.length = 0
   },
 }
