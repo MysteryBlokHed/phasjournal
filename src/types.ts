755d7a4f122abcd8a5b222a7cffe72b106b7ac72
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-08-26 15:04:45 -0400

    Update types.ts
    Adds the new evidence type to the Evidence enum, adds the two new ghosts
    to the list of ghosts, and updates the changed evidence for other
    ghosts.

--- src/types.ts
+++ src/types.ts
@@ -5,6 +5,7 @@ export enum Evidence {
   GhostOrbs = 'Ghost Orbs',
   GhostWriting = 'Ghost Writing',
   Freezing = 'Freezing Temperatures',
+  DotsProjector = 'D.O.T.S Projector',
 }
 
 export interface Ghost {
@@ -17,91 +18,115 @@ export interface Ghost {
 export const AllGhosts: Array<Ghost> = [
   {
     type: 'Spirit',
-    evidence: [
-      Evidence.SpiritBox,
-      Evidence.Fingerprints,
-      Evidence.GhostWriting,
-    ],
+    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostWriting],
     strength: 'None',
     weakness: 'Smudge sticks will stop attacks for a long period of time',
   },
   {
     type: 'Wraith',
-    evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.Freezing],
+    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.DotsProjector],
     strength: `Can't be tracked by footsteps`,
     weakness: 'Toxic reaction to salt',
   },
   {
     type: 'Phantom',
-    evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.Freezing],
+    evidence: [
+      Evidence.SpiritBox,
+      Evidence.Fingerprints,
+      Evidence.DotsProjector,
+    ],
     strength: 'Looking at a it significantly lowers your sanity',
     weakness: 'Taking a photo will make it go away for some time',
   },
   {
     type: 'Poltergeist',
-    evidence: [Evidence.SpiritBox, Evidence.Fingerprints, Evidence.GhostOrbs],
+    evidence: [
+      Evidence.SpiritBox,
+      Evidence.Fingerprints,
+      Evidence.GhostWriting,
+    ],
     strength: 'Can throw many objects at once',
     weakness: 'Almost ineffective in an empty room',
   },
   {
     type: 'Banshee',
-    evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.Freezing],
+    evidence: [
+      Evidence.Fingerprints,
+      Evidence.GhostOrbs,
+      Evidence.DotsProjector,
+    ],
     strength: 'Only targets one person at a time',
     weakness: 'Less active near a Crucifix',
   },
   {
     type: 'Jinn',
-    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostOrbs],
+    evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.Freezing],
     strength: 'Travels faster when its victim is farther away',
     weakness: 'Disabling the breaker disables its ability',
   },
   {
     type: 'Mare',
-    evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.Freezing],
+    evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.GhostWriting],
     strength: 'Increased chance of attacking in the dark',
     weakness: 'Turning lights on will make it less likely to attack',
   },
   {
     type: 'Revenant',
-    evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.GhostWriting],
+    evidence: [Evidence.GhostOrbs, Evidence.GhostWriting, Evidence.Freezing],
     strength: 'Travels faster while hunting',
     weakness: 'Hiding will make it move much slower',
   },
   {
     type: 'Shade',
-    evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.GhostWriting],
+    evidence: [Evidence.Emf5, Evidence.GhostOrbs, Evidence.Freezing],
     strength: 'Being shy makes the ghost harder to find',
     weakness: `Doesn't hunt with multiple people nearby`,
   },
   {
     type: 'Demon',
-    evidence: [Evidence.SpiritBox, Evidence.GhostWriting, Evidence.Freezing],
+    evidence: [Evidence.Fingerprints, Evidence.GhostWriting, Evidence.Freezing],
     strength: 'Attacks more than any other ghost',
     weakness: 'Answered Ouija Board questions do not lower sanity',
   },
   {
     type: 'Yurei',
-    evidence: [Evidence.GhostOrbs, Evidence.GhostWriting, Evidence.Freezing],
+    evidence: [Evidence.GhostOrbs, Evidence.Freezing, Evidence.DotsProjector],
     strength: 'Has a strong effect on sanity',
     weakness:
       'Using Smudge Sticks in the ghost room will stop it wandering for some time',
   },
   {
     type: 'Oni',
-    evidence: [Evidence.Emf5, Evidence.SpiritBox, Evidence.GhostWriting],
+    evidence: [Evidence.Emf5, Evidence.Freezing, Evidence.DotsProjector],
     strength: 'More active with more people and can moves objects fast',
     weakness: 'Being more active makes it easier to find',
   },
   {
     type: 'Hantu',
-    evidence: [Evidence.Fingerprints, Evidence.GhostOrbs, Evidence.GhostWriting],
+    evidence: [
+      Evidence.Fingerprints,
+      Evidence.GhostOrbs,
+      Evidence.GhostWriting,
+    ],
     strength: 'Moves faster at lower temperatures',
     weakness: 'Moves slower at higher temperatures',
   },
   {
     type: 'Yokai',
-    evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.GhostWriting],
+    evidence: [Evidence.SpiritBox, Evidence.GhostOrbs, Evidence.DotsProjector],
     strength: 'Talking near the ghost increses attack chance',
     weakness: 'Can only hear nearby voices when hunting',
+  },
+  {
+    type: 'Goryo',
+    evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.DotsProjector],
+    strength: 'Only appears on camera with nobody nearby',
+    weakness: 'Rarely seen far from ghost room',
+  },
+  {
+    type: 'Myling',
+    evidence: [Evidence.Emf5, Evidence.Fingerprints, Evidence.GhostWriting],
+    strength: 'Quieter while hunting',
+    weakness: 'More frequently makes paranormal sounds',
   },
 ]
